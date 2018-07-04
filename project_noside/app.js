d42e877104438e483845224b7480f8ec8d507c31
ifmoon <ehdals1992@naver.com>
2018-07-04 17:02:53 +0900

    app 수정

--- project_noside/app.js
+++ project_noside/app.js
@@ -1,3 +1,17 @@
 /* router */
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
+
 var app = express();
+app.use(expressLayouts);
+app.set('view engine','ejs');
+
+app.set('layout', 'layout');
+app.set("layout extractScripts", true);
+
+app.get('/',(req, res) =>{
+  res.render('test');
+});
+
+app.listen(3000);
+console.log('conect 3000 port');
