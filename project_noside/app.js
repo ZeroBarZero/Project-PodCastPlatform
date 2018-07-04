f8970720704033a2aa13c1668d5c91a6a6aa2576
ifmoon <ggamangk@gmail.com>
2018-07-05 01:25:27 +0900

    404 페이지 추가

--- project_noside/app.js
+++ project_noside/app.js
@@ -1,29 +1,30 @@
 /* router */
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
-
+var path = require('path');
 var app = express();
-app.use(expressLayouts);
 
-app.set('views', __dirname+'/views');
+app.use(expressLayouts);
+app.use('/css',express.static(path.join(__dirname,'views','_assets','css')));
+app.use('/fonts',express.static(path.join(__dirname,'views','_assets','fonts')));
+app.use('/img',express.static(path.join(__dirname,'views','_assets','img')));
+
+app.set('views', path.join(__dirname,'/views'));
 app.set('view engine','ejs');
 app.set('layout', 'layout');
 app.set("layout extractScripts", true);
 
-
-
 app.get(['/','/:page'],(req, res) => {
   var page = req.params.page;
-  console.log(page);
   if(page){
-    if (page === 'login' ||
-        page === 'register' ||
-        page === 'userInfo'){
-          res.render('user/'+page);
-        }
-
+    if (page === 'login' || page === 'register' || page === 'userInfo'){
+          res.render(path.join('user',page));
+    }
+    else{ // page not found
+      res.render('404')
+    }
   }
-  else{
+  else{ //root
     res.render('index');
   }
 
@@ -32,4 +33,4 @@ app.get(['/','/:page'],(req, res) => {
 
 
 app.listen(3000);
-console.log('conect 3000 port');
+console.log('conect 3000 port :)');
